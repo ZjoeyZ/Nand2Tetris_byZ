b46b92f8b5db41b166123af3eb17e01bcd4fee31
流川枫 <1271418889@qq.com>
2019-09-22 10:03:09 +0800

    delete .name

--- Assembler/src/TestAssembler.java
+++ Assembler/src/TestAssembler.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class TestAssembler {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Assembler assembler = new Assembler();
         try{
             assembler.compile("C:\\Users\\流川枫\\Downloads\\nand2tetris\\projects\\06\\pong\\PongL.asm");
